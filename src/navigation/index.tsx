3395b857fcc9220555e9b56e6a96fed8b8478020
David Acosta <david.acosta.ing@gmail.com>
2025-11-29 15:24:38 -0500

    Deleting template stuff

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -8,59 +8,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Image } from 'react-native';
 import bell from '../assets/bell.png';
 import newspaper from '../assets/newspaper.png';
-import { Home } from './screens/Home';
-import { Profile } from './screens/Profile';
-import { Settings } from './screens/Settings';
-import { Updates } from './screens/Updates';
-import { NotFound } from './screens/NotFound';
 import AddPlace from './screens/AddPlaces';
 import AllPlaces from './screens/AllPlaces';
 
-const HomeTabs = createBottomTabNavigator({
-  screens: {
-    Home: {
-      screen: Home,
-      options: {
-        title: 'Feed',
-        tabBarIcon: ({ color, size }) => (
-          <Image
-            source={newspaper}
-            tintColor={color}
-            style={{
-              width: size,
-              height: size,
-            }}
-          />
-        ),
-      },
-    },
-    Updates: {
-      screen: Updates,
-      options: {
-        tabBarIcon: ({ color, size }) => (
-          <Image
-            source={bell}
-            tintColor={color}
-            style={{
-              width: size,
-              height: size,
-            }}
-          />
-        ),
-      },
-    },
-  },
-});
 
 const RootStack = createNativeStackNavigator({
   screens: {
-    // HomeTabs: {
-    //   screen: HomeTabs,
-    //   options: {
-    //     title: 'Home',
-    //     headerShown: false,
-    //   },
-    // },
+
     AllPlaces: {
       screen: AllPlaces,
       options: {
@@ -72,39 +26,7 @@ const RootStack = createNativeStackNavigator({
       options: {
         title: 'Add New Place',
       },
-    },
-    Profile: {
-      screen: Profile,
-      linking: {
-        path: ':user(@[a-zA-Z0-9-_]+)',
-        parse: {
-          user: (value) => value.replace(/^@/, ''),
-        },
-        stringify: {
-          user: (value) => `@${value}`,
-        },
-      },
-    },
-    Settings: {
-      screen: Settings,
-      options: ({ navigation }) => ({
-        presentation: 'modal',
-        headerRight: () => (
-          <HeaderButton onPress={navigation.goBack}>
-            <Text>Close</Text>
-          </HeaderButton>
-        ),
-      }),
-    },
-    NotFound: {
-      screen: NotFound,
-      options: {
-        title: '404',
-      },
-      linking: {
-        path: '*',
-      },
-    },
+    }
   },
 });
 
